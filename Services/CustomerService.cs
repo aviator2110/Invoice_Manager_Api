80d255fa2cf1ef578271463eca264daaa90818df
Guram Gognidze Work <quram21@gmail.com>
2026-02-05 12:43:40 +0400

    Customer service

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Invoice_Manager_API.Data;
 using Invoice_Manager_API.DTO.CustomerDTO;
+using Invoice_Manager_API.Models;
 using Invoice_Manager_API.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Invoice_Manager_API.Services;
 
@@ -16,28 +18,74 @@ public class CustomerService : ICustomerService
         _mapper = mapper;
     }
 
-    public Task<CustomerResponseDto> CreateAsync(CustomerCreateRequest request)
+    public async Task<CustomerResponseDto> CreateAsync(CustomerCreateRequest request)
     {
-        throw new NotImplementedException();
+        Customer customer = this._mapper.Map<Customer>(request);
+
+        this._context.Customers.Add(customer);
+
+        await this._context.SaveChangesAsync();
+
+        await this._context
+                    .Entry(customer)
+                    .Collection(c => c.Invoices)
+                    .LoadAsync();
+
+        return this._mapper.Map<CustomerResponseDto>(customer);
     }
 
-    public Task<bool> DeleteAsync(int id)
+    public async Task<bool> DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        var customer = await this._context.Customers.FindAsync(id);
+
+        if (customer is null)
+        {
+            return false;
+        }
+
+        this._context.Customers.Remove(customer);
+
+        await this._context.SaveChangesAsync();
+
+        return true;
     }
 
-    public Task<IEnumerable<CustomerResponseDto>> GetAllAsync()
+    public async Task<IEnumerable<CustomerResponseDto>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        var customers = await this._context
+                                        .Customers
+                                        .Include(c => c.Invoices)
+                                        .ToListAsync();
+
+        return this._mapper.Map<IEnumerable<CustomerResponseDto>>(customers);
     }
 
-    public Task<CustomerResponseDto> GetByIdAsync(int id)
+    public async Task<CustomerResponseDto?> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var customer = await this._context.Customers.FindAsync(id);
+
+        if (customer is null)
+        {
+            return null;
+        }
+
+        return this._mapper.Map<CustomerResponseDto>(customer);
     }
 
-    public Task<CustomerResponseDto> UpdateAsync(int id, CustomerUpdateRequest request)
+    public async Task<CustomerResponseDto?> UpdateAsync(int id, CustomerUpdateRequest request)
     {
-        throw new NotImplementedException();
+        var updatedCustomer = await this._context
+                                        .Customers
+                                        .Include(c => c.Invoices)
+                                        .FirstOrDefaultAsync(c => c.Id == id);
+
+        if (updatedCustomer is null)
+        {
+            return null;
+        }
+
+        this._mapper.Map(request, updatedCustomer);
+
+        return this._mapper.Map<CustomerResponseDto>(updatedCustomer);
     }
 }
